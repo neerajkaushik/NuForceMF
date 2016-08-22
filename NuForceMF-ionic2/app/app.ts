b263eaa19937fdb3c8c2476b708c642321a0cd86
Neeraj Kaushik <neerajkaushik@Neerajs-MacBook-Pro.local>
2016-08-22 02:04:11 -0400

    commit operation

---
+++ NuForceMF-ionic2/app/app.ts
@@ -0,0 +1,55 @@
+import {Component, Renderer,ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
+
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = HelloIonicPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController,
+    public renderer: Renderer
+  ) {
+
+    renderer.listenGlobal('document','mfpjsloaded',()=>{
+      console.log("MFP API Init Complete");
+      this.MFPInitComplete();
+    })
+    
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+  MFPInitComplete(){
+        console.log("MFP Init function called");
+        this.initializeApp();
+        // set our app's pages
+        this.pages = [
+          { title: 'Hello Ionic', component: HelloIonicPage },
+          { title: 'My First List', component: ListPage }
+        ];
+  }
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
